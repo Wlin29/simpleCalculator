b909056c31c2c0e9920f68b85327e49881030ad7
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-10-09 19:39:38 +0100

    Added a few more tests

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -47,4 +47,36 @@ public class calculatorTest {
         input = "(aasd)";
         assertEquals("Checking Validation function", false, main.isValidExpression(input));
     }
+
+    @Test
+    public void infixToPostfixTest() {
+        String input = "1*2+3";
+        String output = "12*3+";
+        assertEquals("Checking infixToPostFix function",output, main.infixToPostfix(input));
+        
+        input = "(1+2)*(3-4)";
+        output = "12+34-*";
+        assertEquals("Checking infixToPostFix function",output, main.infixToPostfix(input));
+        
+        input = "1*(2*3+4*5)+6";
+        output = "123*45*+*6+";
+        assertEquals("Checking infixToPostFix function",output, main.infixToPostfix(input));
+    }
+
+    @Test
+    public void evaluatePostfixTest(){
+        String input = "12*3+";
+        int output = 5;
+        assertEquals("Checking evaluate postfix expression",output, main.evaluatePostfix(input));
+
+        input = "22+";
+        output = 4;
+        assertEquals("Checking evaluate postfix expression",output, main.evaluatePostfix(input));
+        
+        input = "11-";
+        output = 0;
+        assertEquals("Checking evaluate postfix expression",output, main.evaluatePostfix(input));
+    }
+
+
 }
