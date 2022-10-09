4afba41dd6783faac7b5a74c9baa930ff67df55d
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-10-09 16:10:06 +0100

    added tests for validation function

--- target/test-classes/calculatorTest.java
+++ target/test-classes/calculatorTest.java
@@ -11,6 +11,39 @@ public class calculatorTest {
     public void validation() {
         String input = "2+2";
         assertEquals("Checking Validation function", true, main.isValidExpression(input));
-        input = "2+2";
+
+        input = "9-2";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+        
+        input = "3*5";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+
+        input = "1-1";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+
+        input = "(1+2)*2";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+
+        input = "-9-3122";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+
+        input = "9*1230+2-5+1";
+        assertEquals("Checking Validation function", true, main.isValidExpression(input));
+
+        input = "9*1230+2-5+One";
+        assertEquals("Checking Validation function", false, main.isValidExpression(input));
+
+        input = "9*shaker";
+        assertEquals("Checking Validation function", false, main.isValidExpression(input));
+
+        input = " ";
+        assertEquals("Checking Validation function", false, main.isValidExpression(input));
+
+        input = "2 + 5 + 3 * 2 - 1";
+        assertEquals("Checking Validation function", false, main.isValidExpression(input));
+
+        input = "(aasd)";
+        assertEquals("Checking Validation function", false, main.isValidExpression(input));
+
     }
 }
